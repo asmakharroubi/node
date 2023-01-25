bebb2efcca74921b206de1eca2fae2bd63466475
asmakharroubi <kharroubiasma@gmail.com>
2023-01-25 18:39:35 +0100

    node

---
+++ reader.js
@@ -0,0 +1,9 @@
+// file-reader.js
+const fs = require("fs");
+
+// create file
+fs.writeFileSync("welcome.txt", "Hello Node");
+
+// read file
+const data = fs.readFileSync("welcome.txt", "utf8");
+console.log(data);
